683bcaf24edc1e3f3475c59649b3992ae6e11334
wellington chida <wellingtonchida@protonmail.com>
2024-01-02 08:12:57 -0300

    domain entity with enum

--- Bookify.Domain/Apartments/Amenity.cs
+++ Bookify.Domain/Apartments/Amenity.cs
@@ -2,5 +2,15 @@
 {
     public enum Amenity
     {
+        WiFi =1,
+        AirConditioning = 2,
+        Parking = 3,
+        PetFriendly = 4,
+        SwimmingPool = 5,
+        Gym = 6,
+        Spa = 7,
+        Terrace = 8,
+        MountainView = 9,
+        GardenView = 10
     }
 }
--- Bookify.Domain/Apartments/Apartment.cs
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -2,10 +2,10 @@
 {
     public sealed class Apartment
     {
+        //amenic because it only a bunch of fields
         public Guid Id { get; private set; }
         public  string Name { get; private set; }
         public string Description { get; private set; }
-
         public string Country { get; private set; }
         public string State { get; private set; }
         public string ZipCode { get; private set; }
@@ -16,5 +16,6 @@
         public decimal CleaningFeeAmount { get; private set; }
         public decimal CleaningFeeCurrency { get; private set; }
         public DateTime? LastBookedOnUtc { get; private set; }
+        public List<Amenity> Amenities { get; private set; } = new();
     }
 }
